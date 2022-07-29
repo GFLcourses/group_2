7e1f29e39af8934ab6c1312ab3fee65a407e8520
chicagoist <le.legioner@gmail.com>
2022-07-30 00:03:42 +0200

    <dependencies:dependency>

--- src/test/java/executor/service/ApplicationTest.java
+++ src/test/java/executor/service/ApplicationTest.java
@@ -2,7 +2,7 @@ package executor.service;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
