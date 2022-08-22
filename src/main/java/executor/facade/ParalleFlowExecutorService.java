f2c7dcb8a2f3af059b2d8b73b574aafd6a82a2d0
G1tch14 <bukachvladislav@gmail.com>
2022-08-22 19:41:47 +0300

    edit Property.java and test

--- src/main/java/executor/facade/ParalleFlowExecutorService.java
+++ src/main/java/executor/facade/ParalleFlowExecutorService.java
@@ -1,5 +1,7 @@
 package executor.facade;
 
+import executor.util.Property;
+
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -8,11 +10,14 @@ import java.util.concurrent.TimeUnit;
 public class ParalleFlowExecutorService {
 
     public void director(){
+       int corePoolSize = Integer.parseInt(Property.getProperty("ThreadPoolConfig.corePoolSize"));
+       long keepAliveTime = Long.parseLong(Property.getProperty("ThreadPoolConfig.keepAliveTime"));
        BlockingQueue<Runnable> blockingQueue =
                 new LinkedBlockingQueue<Runnable>();
 
+
         ThreadPool executor =
-                new ThreadPool(10, 20, 5, TimeUnit.SECONDS,
+                new ThreadPool(corePoolSize, 20, keepAliveTime, TimeUnit.SECONDS,
                         blockingQueue, new ThreadPoolExecutor.AbortPolicy());
 
         // Let start all core threads initially
