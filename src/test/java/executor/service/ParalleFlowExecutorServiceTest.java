bb08a77b62c86d71f1593a6533100aebeb913f28
G1tch14 <bukachvladislav@gmail.com>
2022-08-23 20:33:04 +0300

    fix conflicts + edit test

--- src/test/java/executor/service/ParalleFlowExecutorServiceTest.java
+++ src/test/java/executor/service/ParalleFlowExecutorServiceTest.java
@@ -2,13 +2,20 @@ package executor.service;
 
 
 import executor.facade.ParalleFlowExecutorService;
+import executor.service.impl.StepExecutionServiceClickCss;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static org.mockito.Mockito.*;
 
 public class ParalleFlowExecutorServiceTest {
 
    @Test
     public void PoolTest() {
-       ParalleFlowExecutorService service = new ParalleFlowExecutorService();
-       service.director();
+        ParalleFlowExecutorService service = mock(ParalleFlowExecutorService.class);
+        service.director();
+        verify(service, times(100));
+
    }
 }
