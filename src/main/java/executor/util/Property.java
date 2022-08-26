9a74f6b26737b21bca9291b25387337e80648c00
Bukach Vlad <bukachvladislav@gmail.com>
2022-08-26 23:02:03 +0300

    Update Property.java

--- src/main/java/executor/util/Property.java
+++ src/main/java/executor/util/Property.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Property {
+    //
     protected static FileInputStream fileInputStream;
     protected static Properties PROPERTIES;
     static {
