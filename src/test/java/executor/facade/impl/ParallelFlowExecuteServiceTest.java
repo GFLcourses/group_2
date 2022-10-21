68b50325e15f626f3cf4362eb85eb8ef59770c77
G1tch14 <bukachvladislav@gmail.com>
2022-10-21 19:44:32 +0300

    new branch with ParallelFlowExecuteService

--- src/test/java/executor/facade/impl/ParallelFlowExecuteServiceTest.java
+++ src/test/java/executor/facade/impl/ParallelFlowExecuteServiceTest.java
@@ -1,6 +1,19 @@
 package executor.facade.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import executor.facade.ExecutionService;
+import executor.model.Scenario;
+import executor.service.ExecutorService;
+import executor.service.ScenarioSourceListener;
+import executor.service.impl.ProxySourcesClientService;
+import executor.service.impl.ScenarioExecutorService;
+import executor.service.impl.ScenarioSourceListenerService;
+import executor.util.ObjectMapperUtil;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Queue;
+
 import static org.mockito.Mockito.*;
 
 public class ParallelFlowExecuteServiceTest {
@@ -8,7 +21,12 @@ public class ParallelFlowExecuteServiceTest {
     @Test
     public void parallelTest() {
 
+        ScenarioSourceListenerService scenarioService = new ScenarioSourceListenerService(new ObjectMapperUtil());
+        Queue<Scenario> scenarios = scenarioService.execute();
+
+        ProxySourcesClientService proxy = new ProxySourcesClientService(new ObjectMapper());
         ParallelFlowExecuteService parallelFlowExecuteService = mock(ParallelFlowExecuteService.class);
+        parallelFlowExecuteService.parallelExecute(new Worker(scenarioService,scenarios,proxy));
         verify(parallelFlowExecuteService,times(2));
     }
 }
